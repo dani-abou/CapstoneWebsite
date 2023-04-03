3fde94f3bcfe0fc724e286e0b0e9060f796abb94
dani-abou <abouhamad.d@northeastern.edu>
2023-04-03 15:32:15 -0400

    firebase deployment set up

---
+++ firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBrSRavPhwd1FMWhQ9_BXOCBlb1rcBI0KY",
+  authDomain: "capstone-f733e.firebaseapp.com",
+  projectId: "capstone-f733e",
+  storageBucket: "capstone-f733e.appspot.com",
+  messagingSenderId: "782105461402",
+  appId: "1:782105461402:web:6bac9806b6477cc29ed523"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
