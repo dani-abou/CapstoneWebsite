edb33f23edc69457f0ce1eeb74a783e6eabe2114
Ben Wakefield <wenbakefield@gmail.com>
2023-04-13 00:00:12 -0400

    headshots and some bios

--- src/pages/people/people.js
+++ src/pages/people/people.js
@@ -1,22 +1,50 @@
 export const PEOPLE = [
   {
     fullName: "Dani Abouhamad",
-    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent posuere non sem vitae posuere. Vestibulum massa mi, imperdiet non tellus a, luctus mollis dolor. Quisque ex leo, molestie a eleifend in, cursus efficitur enim. Integer efficitur vel nisl eu dignissim. Donec libero ipsum, facilisis in tellus nec, placerat pretium dui. Sed finibus leo id dolor finibus posuere.",
+    bio: "Dani Abouhamad is a Lebanese American musician raised in Boston Massachusetts. He began his musical journey at 16 years old when he picked up the electric guitar in an obsession with punk rock. Since then, he has founded multiple bands, writes his own music, learned multiple instruments, manages a band, and runs a music venue. In addition to this capstone class, Dani has specialized in unorthodox musical compositions that question your understanding of the artform by playing with the technologies involved.",
   },
   {
-    fullName: "Dani Abouhamad",
+    fullName: "Wilson Dobbs",
     bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent posuere non sem vitae posuere. Vestibulum massa mi, imperdiet non tellus a, luctus mollis dolor. Quisque ex leo, molestie a eleifend in, cursus efficitur enim. Integer efficitur vel nisl eu dignissim. Donec libero ipsum, facilisis in tellus nec, placerat pretium dui. Sed finibus leo id dolor finibus posuere."
   },
   {
-    fullName: "Dani Abouhamad",
+    fullName: "Morgan Forbes",
     bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent posuere non sem vitae posuere. Vestibulum massa mi, imperdiet non tellus a, luctus mollis dolor. Quisque ex leo, molestie a eleifend in, cursus efficitur enim. Integer efficitur vel nisl eu dignissim. Donec libero ipsum, facilisis in tellus nec, placerat pretium dui. Sed finibus leo id dolor finibus posuere."
   },
   {
-    fullName: "Dani Abouhamad",
+    fullName: "Nelwin Gonzalez Barreto",
     bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent posuere non sem vitae posuere. Vestibulum massa mi, imperdiet non tellus a, luctus mollis dolor. Quisque ex leo, molestie a eleifend in, cursus efficitur enim. Integer efficitur vel nisl eu dignissim. Donec libero ipsum, facilisis in tellus nec, placerat pretium dui. Sed finibus leo id dolor finibus posuere."
   },
   {
-    fullName: "Dani Abouhamad",
+    fullName: "Ayla Hadley",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent posuere non sem vitae posuere. Vestibulum massa mi, imperdiet non tellus a, luctus mollis dolor. Quisque ex leo, molestie a eleifend in, cursus efficitur enim. Integer efficitur vel nisl eu dignissim. Donec libero ipsum, facilisis in tellus nec, placerat pretium dui. Sed finibus leo id dolor finibus posuere."
+  },
+  {
+    fullName: "Maggie Kraine",
+    bio: "Maggie Kraine is a third year Music Technology student at Northeastern University.  She was originally a student at University of Massachusetts Amherst for Computer Science, but after taking a few years off school she decided to follow her passion in Music. She’s interested in working in video game sound design or film score composition in the future. She is a member of the Game Design Club and works as a Sound Designer with the Northeastern Theatre Department for their productions."
+  },
+  {
+    fullName: "Sage Langenfeld",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent posuere non sem vitae posuere. Vestibulum massa mi, imperdiet non tellus a, luctus mollis dolor. Quisque ex leo, molestie a eleifend in, cursus efficitur enim. Integer efficitur vel nisl eu dignissim. Donec libero ipsum, facilisis in tellus nec, placerat pretium dui. Sed finibus leo id dolor finibus posuere."
+  },
+  {
+    fullName: "Alex Ma",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent posuere non sem vitae posuere. Vestibulum massa mi, imperdiet non tellus a, luctus mollis dolor. Quisque ex leo, molestie a eleifend in, cursus efficitur enim. Integer efficitur vel nisl eu dignissim. Donec libero ipsum, facilisis in tellus nec, placerat pretium dui. Sed finibus leo id dolor finibus posuere."
+  },
+  {
+    fullName: "Tao Setoguchi",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent posuere non sem vitae posuere. Vestibulum massa mi, imperdiet non tellus a, luctus mollis dolor. Quisque ex leo, molestie a eleifend in, cursus efficitur enim. Integer efficitur vel nisl eu dignissim. Donec libero ipsum, facilisis in tellus nec, placerat pretium dui. Sed finibus leo id dolor finibus posuere."
+  },
+  {
+    fullName: "Josh Tseng",
+    bio: "Josh is an international student from Taiwan. He is currently in his fourth year of study, pursuing a Bachelor of Arts in Music. Josh has a strong passion for orchestral programming and audio engineering. During his co-op experience, he had the opportunity to work at the recording studio at Snell, which marked the beginning of his exciting journey in the field."
+  },
+  {
+    fullName: "Ben Wakefield",
+    bio: "Ben is from Connecticut, and is studying Computer Science & Music Technology. He started playing trumpet in elementary school, switched to percussion in middle school, and picked up the guitar, electric bass, and keyboard in high school. His DAW of choice is Ableton Live. He likes playing and listening to rock, and is a fan of The Doors, Led Zeppelin, The Grateful Dead, and Yes. He’s interested in modular synthesizers, DSP programming, sound restoration, and the intersection of music with AI."
+  },
+  {
+    fullName: "Jerome Washo",
     bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent posuere non sem vitae posuere. Vestibulum massa mi, imperdiet non tellus a, luctus mollis dolor. Quisque ex leo, molestie a eleifend in, cursus efficitur enim. Integer efficitur vel nisl eu dignissim. Donec libero ipsum, facilisis in tellus nec, placerat pretium dui. Sed finibus leo id dolor finibus posuere."
   }
 ]
